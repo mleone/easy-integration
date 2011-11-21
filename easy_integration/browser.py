820df33739acbaac3c68cf85e3dc9ad25056e80a
Mike Leone <michael.leone@panopticdev.com>
2011-11-21 13:10:08 -0500

    Move to more aggressive 0.5 second waiting for rendering.

--- easy_integration/browser.py
+++ easy_integration/browser.py
@@ -17,18 +17,20 @@ def app_url(path):
     return "http://127.0.0.1:8001" + path
 
 def displays(text):
-    sleep(0.2) # Give the browser time to render the page!
+    wait_for_rendering()
     return BROWSER.is_text_present(text)
 
 def click(text):
     """Clicks the first button or link that matches the text.  Buttons have
     priority."""
+    wait_for_rendering()
     match = (BROWSER.find_by_value(text) or 
         BROWSER.find_link_by_text(text) or 
         BROWSER.find_by_css(text))
     return match.first.click()
 
 def fill_in(name, value):
+    wait_for_rendering()
     BROWSER.fill(name, value)
 
 def close():
@@ -36,6 +38,10 @@ def close():
 
 def select(select_id, option_text):
     BROWSER.select(select_id, option_text)
+
+def wait_for_rendering():
+    "Give the browser time to render the page!"
+    sleep(0.5) 
 
 # When the program that imported this module terminates, close the
 # web browser.
