42f418c9c491bea594341532b805713f51f61a1c
Mike Leone <michael.leone@panopticdev.com>
2012-01-15 12:46:21 -0500

    Use chrome as default browser.
    Obviates wait_for_rendering, which was kinda cludgy anyway.

--- easy_integration/browser.py
+++ easy_integration/browser.py
@@ -5,7 +5,7 @@ import atexit
 from splinter.browser import Browser
 from time import sleep
 
-BROWSER = Browser()
+BROWSER = Browser('chrome')
 
 def quit():
     BROWSER.quit()
@@ -17,20 +17,17 @@ def app_url(path):
     return "http://127.0.0.1:8001" + path
 
 def displays(text):
-    wait_for_rendering()
     return BROWSER.is_text_present(text)
 
 def click(text):
     """Clicks the first button or link that matches the text.  Buttons have
     priority."""
-    wait_for_rendering()
     match = (BROWSER.find_by_value(text) or 
         BROWSER.find_link_by_text(text) or 
         BROWSER.find_by_css(text))
     return match.first.click()
 
 def fill_in(name, value):
-    wait_for_rendering()
     BROWSER.fill(name, value)
 
 def close():
@@ -38,10 +35,6 @@ def close():
 
 def select(select_id, option_text):
     BROWSER.select(select_id, option_text)
-
-def wait_for_rendering():
-    "Give the browser time to render the page!"
-    sleep(0.5) 
 
 # When the program that imported this module terminates, close the
 # web browser.
